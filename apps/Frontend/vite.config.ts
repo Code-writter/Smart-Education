2e5a91d24e62e87f9599de7f1fc91681694481f0
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2024-11-01 23:54:26 +0530

    setting up test 4

--- apps/Frontend/vite.config.ts
+++ apps/Frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server : {
     proxy : {
       "/api/v1" : {
-        target : 'http://127.0.0.1:8787/'       
+        target : 'https://backend2.abhishektiwari28032004.workers.dev/'       
       }
     }
   }
